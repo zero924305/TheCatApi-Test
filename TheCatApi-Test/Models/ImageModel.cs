327d12ea52f2322f0ef2f0cb394554b74b080163
Altino-Univar <altino.chong@univarsolutions.com>
2022-06-14 02:04:37 +0100

    Create Image Model

---
+++ TheCatApi-Test/Models/ImageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json.Serialization;
+namespace TheCatApi_Test.Models
+{
+    public class ImageModel
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("height")]
+        public int Height { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("width")]
+        public int Width { get; set; }
+    }
+}
