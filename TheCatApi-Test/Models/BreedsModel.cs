2092fb7d13bf0f10cab6801c729c75837c09adb3
Altino-Univar <altino.chong@univarsolutions.com>
2022-06-14 01:57:31 +0100

    Create Breeds Model

---
+++ TheCatApi-Test/Models/BreedsModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json.Serialization;
+namespace TheCatApi_Test.Models
+{
+    public class BreedsModel
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("life_span")]
+        public string LifeSpan { get; set; }
+
+        [JsonPropertyName("origin")]
+        public string Origin { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("temperament")]
+        public string Temperament { get; set; }
+
+        [JsonPropertyName("short_legs")]
+        public int ShortLegs { get; set; }
+
+        [JsonPropertyName("wikipedia_url")]
+        public Uri WikipediaUrl { get; set; }
+
+        [JsonPropertyName("country_code")]
+        public string CountryCode { get; set; }
+
+        [JsonPropertyName("social_needs")]
+        public int SocialNeeds { get; set; }
+
+        [JsonPropertyName("stranger_friendly")]
+        public int StrangerFriendly { get; set; }
+    }
+}
