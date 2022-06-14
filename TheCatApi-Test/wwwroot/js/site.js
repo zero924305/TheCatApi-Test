76dd53d9dccaad48867ecfed4f7474e0b96ebd64
Altino <altino.chong@univarsolutions.com>
2022-06-14 02:20:12 +0100

    using javascript and jquery to fetch api data

--- TheCatApi-Test/wwwroot/js/site.js
+++ TheCatApi-Test/wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿'use strict'
 
-// Write your JavaScript code.
+//Main Breed Select
+const BreedSelectID = $("#breedSelect");
+
+//Breed Image ID
+const BreedImageID = $("#Breed_image");
+
+//End Point
+const BreedsUrl = 'https://localhost:5001/api/Breeds';
+const ImageUrl  = 'https://localhost:5001/api/Image/breed_id=';
+
+
+//Create Array
+let BreedsData = [];
+let BreedsImageData = [];
+let catData = [];
+
+//Create a async Fetch function
+
+async function getapi(url)
+{
+    const respones = 
+            fetch(url)
+                .then(data=>{return data.json()})
+                .catch(console.error());
+
+    var data = await response;
+
+    if(!BreedsData.length)
+    {
+        BreedsData = data;
+    }
+
+    retrun data;
+}
+
+//Get Breeds Api Data 
+getapi(BreedsUrl);
